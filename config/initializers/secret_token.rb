ed08b1c463c0f8f1285c4b7266ff54d02eeee943
Zooey Nguyen <zooeyn@gmail.com>
2013-06-27 21:24:51 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrailse::Application.config.secret_token = '0bd296e36bb73b03dbd3ff8e99cd8c14f1146ac0d64949cbe8b60a6c10718dbd1edcc245956661420b3d2937416239f25eb64c08385edf65d808572a3882008b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Omrailse::Application.config.session_store :cookie_store, key: '_omrailse_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Omrailse::Application.config.session_store :active_record_store
